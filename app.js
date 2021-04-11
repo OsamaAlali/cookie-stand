c5f4877fffd0a5a83e40964923edbd85467ba5b8
osama-alali <osama.alali89@yahoo.com>
2021-04-11 15:33:40 +0300

    step3

--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const seattle ={
  min :23,
  max :65,
  averg: 6.3,
+ hourSeattle:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+ purchasedCookiesPerHour:[],
  customerPerHour:[],
 gnerateCustomerperhour:function() {
-for (let i=0;i<14;i++)
-    {
-        this.customerPerHour[i]=getRandomIntInclusive(23,65)
+for (let i=0;i<=14;i++)
+    {this.customerPerHour.push(getRandomIntInclusive(this.min,this.max))
+        //this.customerPerHour[i]=getRandomIntInclusive(23,65);
     }
 
 
-}//end function gnerate customar per hour
+},//end function gnerate customar per hour
+gneratePurchasedCookiesPh: function () {
+  console.log(this.customerPerHour)
+    for(let i=0;i<this.customerPerHour.length;i++)
+{
+this.purchasedCookiesPerHour.push(Math.floor((this.averg) *this.customerPerHour[i]));
 
+}//end for loop
+console.log(this.purchasedCookiesPerHour)
+}//end function gneratePurchasedCookiesPh
 
 }//end seattile
-seattle.gnerateCustomerperhour()
+seattle.gnerateCustomerperhour();
+seattle.gneratePurchasedCookiesPh();
 getRandomIntInclusive();
 console.log(seattle.customerPerHour);
+console.log(seattle.purchasedCookiesPerHour)
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
